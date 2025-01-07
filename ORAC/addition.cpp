e8aa336d04d1ed195022d0469453185a379fb0f5
RainFox141 <RainFox141@gmail.com>
2025-01-08 00:20:53 +1000

    Create addition.cpp

---
+++ ORAC/addition.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+int main()
+{
+    freopen("addin.txt", "r", stdin);
+    freopen("addout.txt", "w", stdout);
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+    int a , b;
+    cin >> a >> b;
+    cout << a + b;
+  //the pain of pasting from orac ...
+}
